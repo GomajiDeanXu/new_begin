a7360b52f3ce92423162ae04ceb1a476dac583f8
Dean <jasugun0000@gmail.com>
2019-04-08 12:23:11 +0800

    Log SQL 語句執行內容＆時間

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\Schema;
+use DB;
+use Log;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -23,6 +25,31 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
+
+        DB::listen(function ($query) {
+            $bindings   = $query->bindings;
+            $execTime  = $query->time;
+
+            # 整理格式
+            foreach ($bindings as $i => $binding) {
+                if ($binding instanceof \DateTime) {
+                    $bindings[$i] = $binding->format('Y-m-d H:i:s');
+                } elseif (is_string($binding)) {
+                    $bindings[$i] = str_replace("'", "\\'", $binding);
+                }
+            }
+
+            $execTime = number_format(($execTime/1000.0), 4);
+            $sqlString = str_replace(['%', '?', "\n"], ['%%', "'%s'", ' '], $query->sql);
+            try {
+                $fullString = vsprintf($sqlString, $bindings);
+            } catch (\Exception $e) {
+                $fullString = '';
+                Log::error($e);
+            }
+
+            Log::info('執行時間：' . $execTime . PHP_EOL .'SQL 語句：' . $fullString);
+        });
     }
 }
